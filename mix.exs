573f11a567cd0d2a5d6408c49d912986c3fbeaba
Beltrán José Aceves Gil <beltran.aceves@udc.es>
2021-03-17 19:45:50 +0100

    Fix deps

--- mix.exs
+++ mix.exs
@@ -14,7 +14,7 @@ defmodule Todo.MixProject do
   # Run "mix help compile.app" to learn about applications.
   def application do
     [
-      extra_applications:  [:logger,  :cowboy,  :plug],
+      extra_applications:  [:logger,  :cowboy,  :plug, :eex],
       mod:  {Todo.Application,  []}
     ]
   end
