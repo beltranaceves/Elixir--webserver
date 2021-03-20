c33044750c978bfd88a01b243ba85647c6022544
Beltrán José Aceves Gil <beltran.aceves@udc.es>
2021-03-20 22:42:13 +0100

    Change cache policy

--- priv/static/service-worker.js
+++ priv/static/service-worker.js
@@ -2,5 +2,5 @@ importScripts('https://storage.googleapis.com/workbox-cdn/releases/6.1.1/workbox
 
 workbox.routing.registerRoute(
     ({request}) => request.destination === 'image',
-    new workbox.strategies.CacheFirst()
+    new workbox.strategies.NetworkFirst()
 );
